0a540472db9f719eba5417880567de609bc8ef26
Boanerges Junior <boanerges_junior@hotmail.com>
2021-04-24 18:39:41 -0300

    criando componentes

---
+++ github_api/src/routes/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Switch, Route} from 'react-router'
+
+import Dashboard from '../pages/Dasboard'
+import Repository from '../pages/Repository'
+
+const Routes: React.FC = () => {
+    return (
+        <Switch>
+            <Route path="/" exact component={Dashboard}/>
+            <Route path="/repository" component={Repository}/>
+        </Switch>
+    )
+}
+
+export default Routes
